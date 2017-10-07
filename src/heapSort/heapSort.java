8e0f588638aa3f3a386a375c72a4c3627cfae75b
Daniel Caceres <mattmoocow@live.com>
2017-10-06 17:34:54 -0700

    started max heap

---
+++ src/heapSort/heapSort.java
@@ -0,0 +1,44 @@
+package heapSort;
+
+public class heapSort {
+	public static void main(String[] args) {
+
+	}
+
+	public void maxHeapify(int[] A, int i) {
+		int l = left(i);
+		int r = right(i);
+		int largest;
+		
+		if(l <= A.length && A[l] > A[i]) largest = l;
+		else largest = i;
+
+		if(r <= A.length && A[r] > A[largest]) largest = r;
+		else largest = i;
+		
+		if(largest != i){
+			A[i] = A[largest];
+			maxHeapify(A, largest);
+		}
+		
+		
+	}
+
+	public void buildMaxHeap(int[] A) {
+		A.heap-size = A.length;
+		for(int i = Math.floor(A.length/2))
+	}
+
+	public void heapsort(int[] A) {
+
+	}
+
+	public int left(int i) {
+		return 2 * i;
+	}
+
+	public int right(int i) {
+		return 2 * i + 1;
+	}
+
+}
