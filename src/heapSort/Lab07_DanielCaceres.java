8c9fb0c0471b6a94ca67c280ddf2ad4f25eb4599
Daniel Caceres <mattmoocow@live.com>
2017-10-11 15:24:18 -0700

    fixed heapsort now works

--- src/heapSort/Lab07_DanielCaceres.java
+++ src/heapSort/Lab07_DanielCaceres.java
@@ -1,12 +1,19 @@
 package heapSort;
 
-public class heapSort {
+public class Lab07_DanielCaceres {
+	protected static int heapSize;
+
 	public static void main(String[] args) {
 		int[] A = { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 };
-
+		
+		System.out.print("Before sort\n[ ");
+		for (int a : A)
+			System.out.print(a + " ");
+		System.out.print("]\n");
+		
 		heapsort(A);
-
-		System.out.print("[ ");
+		
+		System.out.print("After sort \n[ ");
 		for (int a : A)
 			System.out.print(a + " ");
 		System.out.print("]");
@@ -17,15 +24,13 @@ public class heapSort {
 		int r = right(i);
 		int largest;
 
-		if (l <= A.length - 1 && A[l] > A[i])
+		if (l <= heapSize  - 1 && A[l] > A[i])
 			largest = l;
 		else
 			largest = i;
 
-		if (r <= A.length - 1 && A[r] > A[largest])
+		if (r <= heapSize - 1 && A[r] > A[largest])
 			largest = r;
-		else
-			largest = i;
 
 		if (largest != i) {
 			int temp = A[i];
@@ -36,8 +41,8 @@ public class heapSort {
 	}
 
 	public static void buildMaxHeap(int[] A) {
-		int heapSize = A.length;
-		for (int i = (int) Math.floor(A.length - 1/ 2); i >=0 ; i--) {
+		heapSize = A.length - 1;
+		for (int i = (int) Math.floor((A.length - 1) / 2); i >= 0; i--) {
 			maxHeapify(A, i);
 		}
 	}
@@ -45,20 +50,20 @@ public class heapSort {
 	public static void heapsort(int[] A) {
 		buildMaxHeap(A);
 		for (int i = A.length - 1; i >= 1; i--) {
-			int temp = A[1];
-			A[1] = A[i];
+			int temp = A[0];
+			A[0] = A[i];
 			A[i] = temp;
-			
+			heapSize = heapSize - 1;
 			maxHeapify(A, 0);
 		}
 	}
 
 	public static int left(int i) {
-		return 2 * i;
+		return 2 * i ;
 	}
 
 	public static int right(int i) {
-		return 2 * i+1;
+		return 2 * i + 1;
 	}
 
 }
