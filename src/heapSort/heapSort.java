d48136ce068c0d80a90d3e49280cfed3e75204cd
Daniel Caceres <mattmoocow@live.com>
2017-10-10 21:50:31 -0700

    changes to heapsort

--- src/heapSort/heapSort.java
+++ src/heapSort/heapSort.java
@@ -2,44 +2,63 @@ package heapSort;
 
 public class heapSort {
 	public static void main(String[] args) {
+		int[] A = { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 };
 
+		heapsort(A);
+
+		System.out.print("[ ");
+		for (int a : A)
+			System.out.print(a + " ");
+		System.out.print("]");
 	}
 
-	public void maxHeapify(int[] A, int i) {
+	public static void maxHeapify(int[] A, int i) {
 		int l = left(i);
 		int r = right(i);
 		int largest;
-		
-		if(l <= A.length && A[l] > A[i]) largest = l;
-		else largest = i;
 
-		if(r <= A.length && A[r] > A[largest]) largest = r;
-		else largest = i;
-		
-		if(largest != i){
-			//to do swap properly.
+		if (l <= A.length - 1 && A[l] > A[i])
+			largest = l;
+		else
+			largest = i;
+
+		if (r <= A.length - 1 && A[r] > A[largest])
+			largest = r;
+		else
+			largest = i;
+
+		if (largest != i) {
+			int temp = A[i];
 			A[i] = A[largest];
+			A[largest] = temp;
 			maxHeapify(A, largest);
 		}
-		
-		
 	}
 
-	public void buildMaxHeap(int[] A) {
-//		A.heap-size = A.length;
-//		for(int i = Math.floor(A.length/2))
+	public static void buildMaxHeap(int[] A) {
+		int heapSize = A.length;
+		for (int i = (int) Math.floor(A.length - 1/ 2); i >=0 ; i--) {
+			maxHeapify(A, i);
+		}
 	}
 
-	public void heapsort(int[] A) {
-
+	public static void heapsort(int[] A) {
+		buildMaxHeap(A);
+		for (int i = A.length - 1; i >= 1; i--) {
+			int temp = A[1];
+			A[1] = A[i];
+			A[i] = temp;
+			
+			maxHeapify(A, 0);
+		}
 	}
 
-	public int left(int i) {
+	public static int left(int i) {
 		return 2 * i;
 	}
 
-	public int right(int i) {
-		return 2 * i + 1;
+	public static int right(int i) {
+		return 2 * i+1;
 	}
 
 }
